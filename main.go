dee0d70394182370233910bbcf033b0b0da789ee
Rory McCune <rory.mccune@datadoghq.com>
2025-05-21 11:24:36 +0100

    Add GoReleaser and GitHub Actions workflow for automated releases

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"github.com/raesene/pasgan/cmd/pasgan"
 )
 
+// Build information. Populated at build-time by GoReleaser.
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	pasgan.Execute()
+	// Pass version information to the cmd package
+	pasgan.Execute(version, commit, date)
 }
